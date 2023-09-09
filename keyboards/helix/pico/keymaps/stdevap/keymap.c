846139cb124eeda5cdc944985259ad11431af8b7
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2023-09-09 11:45:25 +0900

    Swap Adjust and Caps keys

--- keyboards/helix/pico/keymaps/stdevap/keymap.c
+++ keyboards/helix/pico/keymaps/stdevap/keymap.c
@@ -71,14 +71,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust|CtlCMd| Caps |  Opt |CmEisu|Space |TabLwr|EnRais|  BS  |CmKana| Left | Down |  Up  |Right |
+   * | Caps |CtlCMd|Adjust|  Opt |CmEisu|Space |TabLwr|EnRais|  BS  |CmKana| Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
 [_QWERTY] = LAYOUT( \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS, \
       CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                       KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      ADJUST,  CTL_CMD, KC_CAPS, KC_LALT, CMD_EISU, KC_SPC, TAB_LOWR, ENT_RAIS, KC_BSPC,  CMD_KANA, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_CAPS, CTL_CMD, ADJUST,  KC_LALT, CMD_EISU, KC_SPC, TAB_LOWR, ENT_RAIS, KC_BSPC,  CMD_KANA, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Qwerty(Win)
@@ -89,14 +89,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust| App  | Caps |  Win |AltMhn|Space |TabLwr|EnRais|  BS  |AltHnk| Left | Down |  Up  |Right |
+   * | Caps | App  |Adjust|  Win |AltMhn|Space |TabLwr|EnRais|  BS  |AltHnk| Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
 [_QWERTY_W] = LAYOUT( \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS, \
       CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                       KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      ADJUST,  KC_APP,  KC_CAPS, KC_LGUI, ALT_MHEN, KC_SPC, TAB_LOWR, ENT_RAIS, KC_BSPC,  ALT_HENK, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_CAPS, KC_APP,  ADJUST,  KC_LGUI, ALT_MHEN, KC_SPC, TAB_LOWR, ENT_RAIS, KC_BSPC,  ALT_HENK, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
@@ -107,14 +107,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |             |  F9  |  F10 |  End |  PDn |  PUp | Home |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      | F11  | F12  |      |      |ShfSpc|      |      |  Del |      | Mute | Next | Prev | Play |
+   * | F11  | F12  |      |      |      |ShfSpc|      |      |  Del |      | Mute | Next | Prev | Play |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, JP_YEN, \
       KC_F1  , KC_F2,   KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_BRK,  \
       KC_F3  , KC_F4,   KC_F5,   KC_F6,   KC_F7 ,  KC_F8,                       KC_F9,   KC_F10,  KC_END,  KC_PGDN, KC_PGUP, KC_HOME, \
-      _______, KC_F11,  KC_F12,  _______, _______, S(KC_SPC), _______, _______, KC_DEL,  _______, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY \
+      KC_F11, KC_F12,  _______,  _______, _______, S(KC_SPC), _______, _______, KC_DEL,  _______, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY \
       ),
 
   /* Raise
@@ -125,14 +125,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |             |  F9  |  F10 |  End |  PDn |  PUp | Home |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      | F11  | F12  |      |      |ShfSpc|      |      |  Del |      | Mute | Vol- | Vol+ | Play |
+   * | F11  | F12  |      |      |      |ShfSpc|      |      |  Del |      | Mute | Vol- | Vol+ | Play |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE, \
       KC_F1  , KC_F2,   KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,                     KC_MINS, KC_EQL , KC_PLUS, KC_UNDS, _______, KC_PSCR, \
       KC_F3  , KC_F4,   KC_F5,   KC_F6,   KC_F7 ,  KC_F8,                       KC_F9,   KC_F10,  KC_END,  KC_PGDN, KC_PGUP, KC_HOME, \
-      _______, KC_F11,  KC_F12,  _______, _______, S(KC_SPC), _______, _______, KC_DEL,  _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY \
+      KC_F11, KC_F12,   _______, _______, _______, S(KC_SPC), _______, _______, KC_DEL,  _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   /* Adjust
