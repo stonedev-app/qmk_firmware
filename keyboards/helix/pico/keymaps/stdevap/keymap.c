41aff2179efb4eaa68dc1ef80e9bb09e1ea789ab
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2023-10-07 11:24:57 +0900

    Moved the Ctrl key to the thumb position(Windows)

--- keyboards/helix/pico/keymaps/stdevap/keymap.c
+++ keyboards/helix/pico/keymaps/stdevap/keymap.c
@@ -88,20 +88,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty(Win)
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Esc  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  -   |
+   * |  Esc |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  -   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Tab  |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * |  Win |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * | Ctl  | WinS |Adjust|  Win |  Alt |Space |TabLwr|EnRais|  BS  |  Hnk | Left | Down |  Up  |Right |
+   * |  Hnk | WinS |Adjust|  Alt | Ctl  |Space |TabLwr|EnRais|  BS  |  Alt | Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
 [_QWERTY_W] = LAYOUT( \
       KC_ESC,  KC_Q,     KC_W,    KC_E,    KC_R,     KC_T,                           KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_MINUS, \
-      KC_TAB,  KC_A,     KC_S,    KC_D,    KC_F,     KC_G,                           KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LGUI, KC_A,     KC_S,    KC_D,    KC_F,     KC_G,                           KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,                           KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      KC_LCTL, WIN_SF_S, ADJUST,  KC_LGUI, KC_LALT,  KC_SPC, TAB_LOWR_W, ENT_RAIS_W, KC_BSPC,  HANZEN,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      HANZEN,  WIN_SF_S, ADJUST,  KC_LALT, KC_LCTL,  KC_SPC, TAB_LOWR_W, ENT_RAIS_W, KC_BSPC,  KC_RALT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower(Mac)
