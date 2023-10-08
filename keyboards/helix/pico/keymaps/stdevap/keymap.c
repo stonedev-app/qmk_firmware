7e249c65f33e3057ad4306bdbe00aaa21baad287
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2023-10-08 23:15:05 +0900

    Removed arrow keys and adjusted the position of function keys and numeric keys(Win)

--- keyboards/helix/pico/keymaps/stdevap/keymap.c
+++ keyboards/helix/pico/keymaps/stdevap/keymap.c
@@ -97,14 +97,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |  Hnk | WinS |Adjust|  Alt | Ctl  |Space |TabLwr|EnRais|  BS  |  Alt | Left | Down |  Up  |Right |
+   * |  Hnk | WinS |Adjust|AltEs | Ctl  |Space |TabLwr|EnRais|  BS  | Ctl  |AltKna|   `  |   \  |  =   |
    * `-------------------------------------------------------------------------------------------------'
    */
 [_QWERTY_W] = LAYOUT( \
-      KC_ESC,  KC_Q,     KC_W,    KC_E,    KC_R,     KC_T,                           KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_MINUS, \
-      KC_LGUI, KC_A,     KC_S,    KC_D,    KC_F,     KC_G,                           KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,                           KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      HANZEN,  WIN_SF_S, ADJUST,  KC_LALT, KC_LCTL,  KC_SPC, TAB_LOWR_W, ENT_RAIS_W, KC_BSPC,  KC_RALT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_ESC,  KC_Q,     KC_W,    KC_E,      KC_R,     KC_T,                           KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,    KC_MINUS, \
+      KC_LGUI, KC_A,     KC_S,    KC_D,      KC_F,     KC_G,                           KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,     KC_X,    KC_C,      KC_V,     KC_B,                           KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, \
+      HANZEN,  WIN_SF_S, ADJUST,  OPT_EISU,  KC_LCTL,  KC_SPC, TAB_LOWR_W, ENT_RAIS_W, KC_BSPC,  KC_RCTL,  OPT_KANA, KC_GRV,  KC_BSLS, KC_EQUAL \
       ),
 
   /* Lower(Mac)
@@ -127,20 +127,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Lower(Win)
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |  \   |
+   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |PageUp|   +  |   =  |      |             | Left | Down |  Up  |Right | Home | End  |
+   * |      |   !  |   @  |   #  |   $  |   %  |             | Left | Down |  Up  |Right | Home | End  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |PageDn|      |      |   {  |             |   }  |      |      |   [  |   ]  |      |
+   * |      |   ^  |   &  |   *  |   (  |   )  |             | PrSc |   [  |   ]  |   (  |   )  |      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |  Del |      | Mute | Next | Prev | Play |
+   * |      |      |      |      |      |      |      |      |  Del |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER_W] = LAYOUT( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, \
-      _______, _______, KC_PGUP, KC_PLUS, KC_EQL , _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END , \
-      _______, _______, KC_PGDN, _______, _______, KC_LCBR,                   KC_RCBR, _______, _______, KC_LBRC, KC_RBRC, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, KC_MUTE, KC_MNXT, KC_MPRV, KC_MPLY \
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+      _______, KC_EXLM,   KC_AT, KC_HASH, KC_DLR,  KC_PERC,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END , \
+      _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                   KC_PSCR, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______ \
       ),
 
   /* Raise(Mac)
@@ -163,20 +163,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Raise(Win)
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  |  F10 |      |
+   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  |      |
+   * |      |   1  |   2  |   3  |   4  |   5  |             |  F5  |  F6  |  F7  |  F9  |  F8  |  F10 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F11 |  F12 |      |      |      |             | PrSc |      |      |      |      |      |
+   * |      |   6  |   7  |   8  |   9  |   0  |             | PrSc |   [  |   ]  |   (  |   )  |      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      | Mute | Vol- | Vol+ | Play |
+   * |      |      |      |      |      |      |      |      |  Del |      |      |      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE_W] = LAYOUT( \
-      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-      _______, KC_F11,  KC_F12,  _______, _______, _______,                     KC_PSCR, _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY \
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, \
+      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_F5,   KC_F6,   KC_F7,   KC_F9,   KC_F8,  KC_F10, \
+      _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                   KC_PSCR, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______,  KC_DEL, _______, _______, _______, _______, _______ \
       ),
 
   /* Adjust
