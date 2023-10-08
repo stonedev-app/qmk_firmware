4effeef57742eba890ff33d03126a7597501a2f5
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2023-10-08 22:59:02 +0900

    Aligned the spaces

--- keyboards/helix/pico/keymaps/stdevap/keymap.c
+++ keyboards/helix/pico/keymaps/stdevap/keymap.c
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
 [_QWERTY] = LAYOUT( \
-      KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_MINUS, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,                       KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+      KC_ESC,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,                       KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,    KC_MINUS, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,                       KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,                       KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, \
       KC_CAPS, KC_MCTL, ADJUST,  OPT_EISU, KC_LGUI,  KC_SPC, TAB_LOWR, ENT_RAIS, KC_BSPC,  KC_RGUI,  OPT_KANA, KC_GRV,  KC_BSLS, KC_EQUAL \
       ),
 
