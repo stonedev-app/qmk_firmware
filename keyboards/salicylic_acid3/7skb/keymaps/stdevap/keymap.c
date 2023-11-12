53cfe40c7b097d33353ab997c4da06ec3ba173d4
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2023-11-12 17:19:38 +0900

    Added functionality to convert ANSI to JIS (Note: Windows doesn't allow setting ANSI or JIS per keyboard)

--- keyboards/salicylic_acid3/7skb/keymaps/stdevap/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/stdevap/keymap.c
@@ -1,4 +1,6 @@
 #include QMK_KEYBOARD_H
+#include "lib/a2j/translate_ansi_to_jis.h"
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -89,4 +91,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_effect_range( 1, 11);
 #endif
 return state;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // Convert ANSI to JIS
+  return process_record_user_a2j(keycode, record);
 }
--- keyboards/salicylic_acid3/7skb/keymaps/stdevap/rules.mk
+++ keyboards/salicylic_acid3/7skb/keymaps/stdevap/rules.mk
@@ -1,1 +1,3 @@
 VIA_ENABLE = yes
+
+SRC += lib/a2j/translate_ansi_to_jis.c
