c887bb5653afb87ea6303efc8338e2bf9c0c7590
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2024-01-02 17:25:04 +0900

    Adjusting timing for tap and hold

--- keyboards/helix/pico/keymaps/stdevapvia/config.h
+++ keyboards/helix/pico/keymaps/stdevapvia/config.h
@@ -22,6 +22,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // place overrides here
 
+#undef TAPPING_TERM
+#define TAPPING_TERM 300
+#define PERMISSIVE_HOLD
+/* when TAPPING_TERM >= 500 same effect PERMISSIVE_HOLD.
+   see tmk_core/common/action_tapping.c */
+
 #ifdef AUDIO_ENABLE
   #define STARTUP_SONG SONG(STARTUP_SOUND)
   #define AUDIO_CLICKY
